0c853e95289b2620ad76adc38df76f5552074442
Gopi Chand <gopi.chand@aajtak.com>
2024-04-13 00:05:52 +0530

    updated for test

--- src/components/home/banner.js
+++ src/components/home/banner.js
@@ -35,7 +35,7 @@ export default function Banner() {
                   <span className="rbt-new-badge-icon">
                     <PersonSearchIcon sx={{ color: "#228756", fontSize: 30 }} />
                   </span>{" "}
-                  Discover mental wellness solutions.
+                  Discovery channel mental wellness solutions.
                 </div>
                 <h4 class="title">
                   Find your therapist <br />
