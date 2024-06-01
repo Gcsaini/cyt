281b209f773c78154721c99a64debb9aa8dd4c29
Deepak <dpk7697@gmail.com>
2024-06-02 02:05:54 +0530

    updated content on <home>

--- src/components/home/banner.js
+++ src/components/home/banner.js
@@ -50,9 +50,9 @@ export default function Banner() {
                   className={isMobile ? "banner-text-title" : "title"}
                   style={{ marginTop: isMobile ? 10 : 20 }}
                 >
-                  Find a way and start journey 
+                  Start Wellness Journey with 
                   <br />
-                  with #ChooseYourTherapist! We provide &nbsp; 
+                 #ChooseYourTherapist! We provide &nbsp; 
                   <span className="header-caption ms-2">
                     <span className="cd-headline rotate-1">
                       <span className="cd-words-wrapper">
@@ -78,7 +78,7 @@ export default function Banner() {
                     </span>
                   </span>
                   <br />
-                  mental health solutions.
+                  Mental Health Solutions.
                 </h4>
                 <p className="description">
                   we provide mental health experts every step of the way to your
