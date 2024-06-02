b5ba413232f9ac1bf47c6dc75394be057595b245
Deepak <dpk7697@gmail.com>
2024-06-03 02:45:56 +0530

    updated content on <home>

--- src/components/home/counter.js
+++ src/components/home/counter.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import CounterImg from "../../assets/img/counter-01.webp";
-import CounterImg2 from "../../assets/img/counter-02.webp";
+import CounterImg from "../../assets/img/counter-01.png";
+import CounterImg2 from "../../assets/img/counter-02.png";
 import CounterImg3 from "../../assets/img/counter-03.webp";
 import CounterImg4 from "../../assets/img/counter-04.webp";
 import LazyImage from "../../utils/lazy-image";
