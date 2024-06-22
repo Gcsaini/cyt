75f03935a7a74d38e416f83433ffef0a96556f33
Gopi Chand <gopi.chand@aajtak.com>
2024-06-22 23:37:22 +0530

    fixed deploy issue

--- src/components/view_profile/profile-info-tab.js
+++ src/components/view_profile/profile-info-tab.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 export default function ProfileInfoTab() {
   const [tab, setTab] = React.useState(1);
   const handleClick = (id) => {
