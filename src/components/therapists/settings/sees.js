9426c9870403c6b3bd3dd4d592f22cfc0c2a39d8
Sooraj Singh <soorajsingh7505@gmail.com>
2024-06-26 16:46:29 +0530

    upload

--- src/components/therapists/settings/sees.js
+++ src/components/therapists/settings/sees.js
@@ -1,9 +1,121 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaInfoCircle, FaMicrophone, FaVideo, FaUser } from "react-icons/fa";
+import "./sees.css";
 
-const sees = () => {
+const Sees = () => {
+  const counsellingTypes = [
+    "Individual Counselling",
+    "Couple Counselling",
+    "Teen Counselling"
+  ];
+
+  const [prices, setPrices] = useState(
+    counsellingTypes.reduce((acc, type) => {
+      acc[type] = { audio: "", video: "", inPerson: "" };
+      return acc;
+    }, {})
+  );
+
+  const handleChange = (type, field, value) => {
+    setPrices((prevPrices) => ({
+      ...prevPrices,
+      [type]: {
+        ...prevPrices[type],
+        [field]: value
+      }
+    }));
+  };
+
   return (
-    <div>sees</div>
-  )
-}
+    <>
+      <div className="sees-container">
+        <div className="table-wrapper">
+          <table className="sees-table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>
+                  Audio <FaMicrophone className="icon" />
+                </th>
+                <th>
+                  Video <FaVideo className="icon" />
+                </th>
+                <th>
+                  In-person <FaUser className="icon" />
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {counsellingTypes.map((type) => (
+                <tr key={type}>
+                  <td>
+                    {type}{" "}
+                    <FaInfoCircle
+                      title={`Short information about ${type}`}
+                      className="info-icon"
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      placeholder="Price"
+                      value={prices[type].audio}
+                      onChange={(e) =>
+                        handleChange(
+                          type,
+                          "audio",
+                          parseFloat(e.target.value) || ""
+                        )
+                      }
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      placeholder="Price"
+                      value={prices[type].video}
+                      onChange={(e) =>
+                        handleChange(
+                          type,
+                          "video",
+                          parseFloat(e.target.value) || ""
+                        )
+                      }
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="number"
+                      placeholder="Price"
+                      value={prices[type].inPerson}
+                      onChange={(e) =>
+                        handleChange(
+                          type,
+                          "inPerson",
+                          parseFloat(e.target.value) || ""
+                        )
+                      }
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
 
-export default sees
+      <div className="col-12 mt--10">
+        <div className="rbt-form-group">
+          <a
+            className="rbt-btn btn-gradient"
+            href="/instructor/instructor-settings#"
+          >
+            Save
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sees;
