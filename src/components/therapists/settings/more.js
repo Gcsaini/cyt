ea33244a31da94468cf1edfa3d7d91051a167b53
Sooraj Singh <soorajsingh7505@gmail.com>
2024-06-27 13:34:41 +0530

    upload

--- src/components/therapists/settings/more.js
+++ src/components/therapists/settings/more.js
@@ -3,6 +3,11 @@ import { languageSpoken } from "../../../utils/static-lists";
 
 export default function More() {
   const [selectedLanguages, setSelectedLanguages] = useState([]);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
   const handleLanguageChange = (event) => {
     const { value } = event.target;
@@ -12,24 +17,40 @@ export default function More() {
       setSelectedLanguages([...selectedLanguages, value]);
     }
   };
-  // console.log(selectedLanguages);
 
   return (
     <div className="container mt-3">
       <h4>Select Languages</h4>
-      <select
-        className="form-select"
-        multiple
-        value={selectedLanguages}
-        onChange={handleLanguageChange}
-        style={{ height: "150px", overflowY: "auto", fontSize: "15px" }} // Inline styles for height and scrolling
-      >
-        {languageSpoken.map((language) => (
-          <option key={language} value={language}>
-            {language}
-          </option>
-        ))}
-      </select>
+      <div className="dropdown">
+        <button
+          className="btn btn-secondary dropdown-toggle"
+          type="button"
+          onClick={toggleDropdown}
+        >
+          {selectedLanguages.length > 0
+            ? selectedLanguages.join(", ")
+            : "Select languages"}
+        </button>
+        <ul
+          className={`dropdown-menu ${dropdownOpen ? "show" : ""}`}
+          style={{ maxHeight: "150px", overflowY: "auto", fontSize: "15px" }}
+        >
+          {languageSpoken.map((language) => (
+            <li key={language}>
+              <input
+                type="checkbox"
+                id={language}
+                value={language}
+                onChange={handleLanguageChange}
+                checked={selectedLanguages.includes(language)}
+              />
+              <label htmlFor={language} className="ml-2">
+                {language}
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
       <div className="mt-3">
         <h5>Selected Languages:</h5>
         {selectedLanguages.length > 0 ? (
