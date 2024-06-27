b4047cd50db917518002535c66f7209a0b72de5c
Sooraj Singh <soorajsingh7505@gmail.com>
2024-06-27 17:38:32 +0530

    upload

--- src/components/therapists/settings/more.js
+++ src/components/therapists/settings/more.js
@@ -20,38 +20,37 @@ export default function More() {
   };
 
   return (
-    <>
-          <div className="container mt-3">
-            <div className="dropdown">
-              <button
-                className="btn btn-secondary dropdown-toggle"
-                type="button"
-                onClick={toggleDropdown}
-              >
-                {selectedLanguages.length > 0
-                  ? selectedLanguages.join(", ")
-                  : "Select languages"}
-              </button>
-              <div>
-                <ul className={`dropdown-menu ${dropdownOpen ? "show" : ""}`}>
-                  {languageSpoken.map((language) => (
-                    <li key={language}>
-                      <input
-                        type="checkbox"
-                        id={language}
-                        value={language}
-                        onChange={handleLanguageChange}
-                        checked={selectedLanguages.includes(language)}
-                      />
-                      <label htmlFor={language} className="ml-2">
-                        {language}
-                      </label>
-                    </li>
-                  ))}
-                </ul>
+    <div className="container mt-3">
+      <div className="dropdown">
+        <button
+          className="btn btn-secondary dropdown-toggle"
+          type="button"
+          onClick={toggleDropdown}
+        >
+          {selectedLanguages.length > 0
+            ? selectedLanguages.join(", ")
+            : "Select languages"}
+        </button>
+        <ul className={`dropdown-menu ${dropdownOpen ? "show" : ""}`}>
+          {languageSpoken.map((language) => (
+            <li key={language} className="dropdown-item">
+              <div className="form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id={language}
+                  value={language}
+                  onChange={handleLanguageChange}
+                  checked={selectedLanguages.includes(language)}
+                />
+                <label htmlFor={language} className="form-check-label">
+                  {language}
+                </label>
               </div>
-            </div>
-          </div>
-    </>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
