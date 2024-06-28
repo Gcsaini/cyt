f1ba24abac398ab4aebb86a0c4dd9242fff27225
Sooraj Singh <soorajsingh7505@gmail.com>
2024-06-28 16:10:41 +0530

    upload

--- src/components/therapists/dashboard/counts.js
+++ src/components/therapists/dashboard/counts.js
@@ -1,11 +1,15 @@
 import "./counts.css";
+import { FaInfoCircle } from "react-icons/fa";
 
 export default function TotalCounts() {
   return (
     <div className="rbt-dashboard-content bg-color-white rbt-shadow-box mb--60">
       <div className="content">
         <div className="section-title">
-          <h4 className="rbt-title-style-3">Performance </h4>
+          <h4 className="rbt-title-style-3">
+            Performance{" "}
+            <FaInfoCircle title="Short information about performance" />
+          </h4>
           <h6>Last 28 days</h6>
         </div>
         <div className="row g-5">
