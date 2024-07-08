91f964156adaf8347d1c14f8a6ed3264f38f6ed2
Sooraj Singh <soorajsingh7505@gmail.com>
2024-07-08 21:56:13 +0530

    u

--- src/pages/therapists/dashboard.js
+++ src/pages/therapists/dashboard.js
@@ -1,19 +1,18 @@
 import React from "react";
 import MainLayout from "../../components/therapists/main-layout";
-import TotalCounts from "../../components/therapists/dashboard/counts";
 import Appointment from "../../components/therapists/dashboard/appointment-main";
 import Upcomingappointment from "../../components/therapists/dashboard/upcommingappointment";
 import RecentInvoices from "../../components/therapists/dashboard/recentInvoices";
 import Notification from "../../components/therapists/dashboard/notifications";
+import PerformanceComponent from "../../components/therapists/dashboard/performance";
 export default function TherapistDashboard() {
   return (
     <MainLayout>
-      <TotalCounts />
-     
-      {/*  <Notification />*/}
+      <PerformanceComponent />
+       <Notification />
       <Upcomingappointment />
       <Appointment />
-     
+
       <RecentInvoices />
     </MainLayout>
   );
