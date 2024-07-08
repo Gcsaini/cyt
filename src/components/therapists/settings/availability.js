602d4d2bfe3f09ba2a0413a9bd26e569d8778cdc
Sooraj Singh <soorajsingh7505@gmail.com>
2024-07-08 14:30:11 +0530

    Update availability.js

--- src/components/therapists/settings/availability.js
+++ src/components/therapists/settings/availability.js
@@ -1,9 +1,88 @@
 import React, { useState } from "react";
 
 const Availability = () => {
+  const initialTimes = {
+    Monday: [{ open: "", close: "" }],
+    Tuesday: [{ open: "", close: "" }],
+    Wednesday: [{ open: "", close: "" }],
+    Thursday: [{ open: "", close: "" }],
+    Friday: [{ open: "", close: "" }],
+    Saturday: [{ open: "", close: "" }],
+    Sunday: [{ open: "", close: "" }]
+  };
+
+  const [times, setTimes] = useState(initialTimes);
+
+  const allTimes = [
+    "12:00am",
+    "01:00am",
+    "01:30am",
+    "02:00am",
+    "02:30am",
+    "03:00am",
+    "03:30am",
+    "04:00am",
+    "04:30am",
+    "05:00am",
+    "05:30am",
+    "06:00am",
+    "06:30am",
+    "07:00am",
+    "07:30am",
+    "08:00am",
+    "08:30am",
+    "09:00am",
+    "09:30am",
+    "10:00am",
+    "10:30am",
+    "11:00am",
+    "11:30am",
+    "12:00pm",
+    "01:00pm",
+    "01:30pm",
+    "02:00pm",
+    "02:30pm",
+    "03:00pm",
+    "03:30pm",
+    "04:00pm",
+    "04:30pm",
+    "05:00pm",
+    "05:30pm",
+    "06:00pm",
+    "06:30pm",
+    "07:00pm",
+    "07:30pm",
+    "08:00pm",
+    "08:30pm",
+    "09:00pm",
+    "09:30pm",
+    "10:00pm",
+    "10:30pm",
+    "11:00pm",
+    "11:30pm"
+  ];
+
+  const handleTimeChange = (day, index, type, event) => {
+    const newTimes = { ...times };
+    newTimes[day][index][type] = event.target.value;
+    setTimes(newTimes);
+  };
+
+  const addOvertime = (day) => {
+    const newTimes = { ...times };
+    newTimes[day].push({ open: "", close: "" });
+    setTimes(newTimes);
+  };
+
+  const deleteOvertime = (day, index) => {
+    const newTimes = { ...times };
+    newTimes[day].splice(index, 1);
+    setTimes(newTimes);
+  };
+
   return (
     <>
-      <table class="rbt-table table table-borderless">
+      <table className="rbt-table table table-borderless">
         <thead>
           <tr>
             <th>Days</th>
@@ -13,153 +92,76 @@ const Availability = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th>Monday</th>
-            <td>
-              <div class="col-lg-6">
-                <div class="rbt-modern-select bg-transparent height-45">
-                  <div class="react-select css-b62m3t-container">
-                    <span
-                      id="react-select-sortBySelect-live-region"
-                      class="css-7pg0cj-a11yText"
-                    ></span>
-                    <span
-                      aria-live="polite"
-                      aria-atomic="false"
-                      aria-relevant="additions text"
-                      role="log"
-                      class="css-7pg0cj-a11yText"
-                    ></span>
-                    <div class="react-select__control css-13cymwt-control">
-                      <div class="react-select__value-container react-select__value-container--has-value css-hlgwow">
-                        <div class="react-select__single-value css-1dimb5e-singleValue">
-                          Tongi
-                        </div>
-                        <div
-                          class="react-select__input-container css-19bb58m"
-                          data-value=""
+          {Object.keys(times).map((day) => (
+            <React.Fragment key={day}>
+              {times[day].map((time, index) => (
+                <tr key={`${day}-${index}`}>
+                  <th>{index === 0 ? day : ""}</th>
+                  <td>
+                    <div className="col-lg-6">
+                      <div className="rbt-modern-select bg-transparent height-45">
+                        <select
+                          className="form-select"
+                          value={time.open}
+                          onChange={(event) =>
+                            handleTimeChange(day, index, "open", event)
+                          }
+                          style={{ padding: "10px" }}
                         >
-                          <input
-                            class="react-select__input"
-                            style={{
-                              color: "inherit",
-                              background: "0px center",
-                              opacity: 0,
-                              width: "100%",
-                              gridArea: "1 / 2",
-                              font: "inherit",
-                              minWidth: "2px",
-                              border: "0px",
-                              margin: "0px",
-                              outline: "0px",
-                              padding: "0px"
-                            }}
-                            autocapitalize="none"
-                            autocomplete="off"
-                            autocorrect="off"
-                            id="react-select-sortBySelect-input"
-                            spellcheck="false"
-                            tabindex="0"
-                            type="text"
-                            aria-autocomplete="list"
-                            aria-expanded="false"
-                            aria-haspopup="true"
-                            role="combobox"
-                            aria-activedescendant=""
-                            value=""
-                          />
-                        </div>
-                      </div>
-                      <div class="react-select__indicators css-1wy0on6">
-                        <span class="react-select__indicator-separator css-1u9des2-indicatorSeparator"></span>
-                        <div
-                          class="react-select__indicator react-select__dropdown-indicator css-1xc3v61-indicatorContainer"
-                          aria-hidden="true"
-                        >
-                          <svg
-                            height="20"
-                            width="20"
-                            viewBox="0 0 20 20"
-                            aria-hidden="true"
-                            focusable="false"
-                            class="css-8mmkcg"
-                          >
-                            <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
-                          </svg>
-                        </div>
+                          <option value="">Select Time</option>
+                          {allTimes.map((timeOption) => (
+                            <option key={timeOption} value={timeOption}>
+                              {timeOption}
+                            </option>
+                          ))}
+                        </select>
                       </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td>January 27, 2022</td>
-            <td>
-              <a class="rbt-btn btn-gradient btn-sm" href="/my-account#">
-                <i className="fa-solid fa-plus"></i>
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <th>Tuesday</th>
-            <td>Priyanka Tiwari</td>
-            <td>January 27, 2022</td>
-            <td>
-              <a class="rbt-btn btn-gradient btn-sm" href="/my-account#">
-                <i className="fa-solid fa-plus"></i>
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <th>Wednesday</th>
-            <td>Ravikant</td>
-            <td>January 27, 2022</td>
-            <td>
-              <a class="rbt-btn btn-gradient btn-sm" href="/my-account#">
-                <i className="fa-solid fa-plus"></i>
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <th>Thursday</th>
-            <td>Lavnya Singh</td>
-            <td>January 27, 2022</td>
-            <td>
-              <a class="rbt-btn btn-gradient btn-sm" href="/my-account#">
-                <i className="fa-solid fa-plus"></i>
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <th>Friday</th>
-            <td>Lavnya Singh</td>
-            <td>January 27, 2022</td>
-            <td>
-              <a class="rbt-btn btn-gradient btn-sm" href="/my-account#">
-                <i className="fa-solid fa-plus"></i>
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <th>Saturday</th>
-            <td>Lavnya Singh</td>
-            <td>January 27, 2022</td>
-            <td>
-              <a class="rbt-btn btn-gradient btn-sm" href="/my-account#">
-                <i className="fa-solid fa-plus"></i>
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <th>Sunday</th>
-            <td>Lavnya Singh</td>
-            <td>January 27, 2022</td>
-            <td>
-              <a class="rbt-btn btn-gradient btn-sm" href="/my-account#">
-                <i className="fa-solid fa-plus"></i>
-              </a>
-            </td>
-          </tr>
+                  </td>
+                  <td>
+                    <div className="col-lg-6">
+                      <div className="rbt-modern-select bg-transparent height-45">
+                        <select
+                          className="form-select"
+                          value={time.close}
+                          onChange={(event) =>
+                            handleTimeChange(day, index, "close", event)
+                          }
+                          style={{ padding: "10px" }}
+                        >
+                          <option value="">Select Time</option>
+                          {allTimes.map((timeOption) => (
+                            <option key={timeOption} value={timeOption}>
+                              {timeOption}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    {index === 0 ? (
+                      <a
+                        className="rbt-btn btn-gradient btn-sm"
+                        href="#"
+                        onClick={() => addOvertime(day)}
+                      >
+                        <i className="fa-solid fa-plus"></i>
+                      </a>
+                    ) : (
+                      <a
+                        className="rbt-btn btn-gradient btn-sm"
+                        href="#"
+                        onClick={() => deleteOvertime(day, index)}
+                      >
+                        <i className="fa-solid fa-minus"></i>
+                      </a>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </React.Fragment>
+          ))}
         </tbody>
       </table>
 
