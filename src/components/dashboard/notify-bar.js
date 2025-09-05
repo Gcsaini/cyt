8bcd0356836fec122fd02aff1a21d4968cb06bc6
Deepak <dpk7697@gmail.com>
2025-09-06 01:31:18 +0530

    updated content on <home>

--- src/components/dashboard/notify-bar.js
+++ src/components/dashboard/notify-bar.js
@@ -4,7 +4,7 @@ export default function NotifyBar() {
                 <div class="row align-items-center">
                     <div class="col-lg-4 col-md-5 col-12 d-none d-md-block">
                         <div class="fancy-menu-text fancu-menu-start">
-                            <p>Intro price. Get Histudy for Big Sale -95% off.<i class="feather-chevron-right"></i></p>
+                            <p>This is your safe corner for therapy, learning, and self-discovery.<i class="feather-chevron-right"></i></p>
                         </div>
                     </div>
                 </div>
