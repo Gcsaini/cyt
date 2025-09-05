cef4b0392545f26ddb8d314a29a737bc4bc6fa21
Deepak <dpk7697@gmail.com>
2025-09-05 18:13:01 +0530

    updated content on <home>

--- src/pages/client/user-dashboard.js
+++ src/pages/client/user-dashboard.js
@@ -1,16 +1,15 @@
 import MyDashboard from "../../components/dashboard/my-dashboard";
 import React from "react";
 import UserLayout from "../../components/dashboard/user-layout";
-import NewsLetter from "../components/home/newsletter";
-import Footer from "../components/footer";
+
+
 
 export default function UserDashboard() {
   return (
     <div id="__next">
       <UserLayout>
         <MyDashboard />
-         <NewsLetter />
-  <Footer />
+      
       </UserLayout>
     </div>
   );
