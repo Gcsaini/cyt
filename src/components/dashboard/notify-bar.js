c262e9a3c1386c90ccc11c3540a4409f19e5e2b2
Gcsaini <gopichand0215@gmail.com>
2025-09-06 01:13:43 +0530

    fixed notify bar

--- src/components/dashboard/notify-bar.js
+++ src/components/dashboard/notify-bar.js
@@ -1,5 +1,5 @@
 export default function NotifyBar() {
-    return <div class="rbt-header-top-2  color-white pt--15 pb--15 d-none d-xl-block mb--15" style={{backgroundColor:"darkblue",borderRadius:"6px"}}>
+    return <div class="rbt-header-top-2 color-white pt--15 pb--15 mb--15" style={{backgroundColor:"darkblue",borderRadius:"6px"}}>
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-lg-4 col-md-5 col-12 d-none d-md-block">
