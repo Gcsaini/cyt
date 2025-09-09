241526f1cec4d87299b1e5ffe2fb72783d9ba6b0
Deepak <dpk7697@gmail.com>
2025-09-09 16:18:56 +0530

    updated content on <home>

--- src/components/therapists/dashboard/performance.js
+++ src/components/therapists/dashboard/performance.js
@@ -2,7 +2,7 @@ import { useMediaQuery } from "@mui/material";
 
 export default function NotifyBar({ title = "Good to see you here." }) {
     const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
-    return <div class="rbt-header-top-2 color-white pt--15 pb--15 mb--15" style={{ backgroundColor: "#013220", borderRadius: "6px", marginTop: isMobile ? '20px' : 0 }}>
+    return <div class="rbt-header-top-2 color-white pt--15 pb--15 mb--15" style={{ backgroundColor: "rgba(43, 194, 76, 1)", borderRadius: "6px", marginTop: isMobile ? '20px' : 0 }}>
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-4 col-md-5 col-12">
