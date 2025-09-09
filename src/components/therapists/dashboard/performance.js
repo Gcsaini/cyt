655e8c2082a6b687784ae656be8064afb6d77706
Deepak <dpk7697@gmail.com>
2025-09-09 12:33:01 +0530

    updated content on <home>

--- src/components/therapists/dashboard/performance.js
+++ src/components/therapists/dashboard/performance.js
@@ -1,108 +1,16 @@
-import { FaInfoCircle } from "react-icons/fa";
-import ImageTag from "../../../utils/image-tag";
-import CounterImg from "../../../assets/img/counter-01.png";
-import CounterImg1 from "../../../assets/img/counter-03.png";
-import CounterImg2 from "../../../assets/img/card-icon-11b092.png";
-import CounterImg3 from "../../../assets/img/projectscyt.png";
-export default function PerformanceComponent({ pageData }) {
-  const counterValueStyle = {
-    fontWeight: 700,
-    fontSize: "50px",
-    color: "#192335",
-    LineHeight: "56px",
-    padding: "0 24px",
-    marginBottom: "9px",
-  };
-  return (
-    <>
-      <div className="section-title">
-        <h4 className="rbt-title-style-3">
-          Performance &nbsp;
-          <FaInfoCircle title="Short information about performance" />
-          <h6>Last 28 days</h6>
-        </h4>
-      </div>
+import { useMediaQuery } from "@mui/material";
 
-      <div className="row g-5">
-        <div className="col-lg-3 col-md-4 col-sm-6 col-12 ">
-          <div className="rbt-counterup rbt-hover-03">
-            <div className="inner">
-              <div className="icon">
-                <ImageTag
-                  alt="Icons Images"
-                  width="100"
-                  height="100"
-                  src={CounterImg2}
-                />
-              </div>
-              <div className="content">
-                <span style={counterValueStyle}>
-                  {pageData.appointments ? pageData.appointments.length : 0}
-                </span>
-                <span className="subtitle">Total Appointments</span>
-              </div>
+export default function NotifyBar({ title = "Good to see you here." }) {
+    const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+    return <div class="rbt-header-top-2 color-white pt--15 pb--15 mb--15" style={{ backgroundColor: "#013220", borderRadius: "6px", marginTop: isMobile ? '20px' : 0 }}>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-4 col-md-5 col-12">
+                    <div class="fancy-menu-text fancu-menu-start">
+                        <p>{title}</p>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-        <div className="col-lg-3 col-md-4 col-sm-6 col-12 ">
-          <div className="rbt-counterup rbt-hover-03">
-            <div className="inner">
-              <div className="icon">
-                <ImageTag
-                  alt="Icons Images"
-                  width="100"
-                  height="100"
-                  src={CounterImg3}
-                />
-              </div>
-              <div className="content">
-                <span style={counterValueStyle}>
-                  {pageData.clients ? pageData.clients.length : 0}
-                </span>
-                <span className="subtitle">Total Clients</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-4 col-sm-6 col-12 ">
-          <div className="rbt-counterup rbt-hover-03">
-            <div className="inner">
-              <div className="icon">
-                <ImageTag
-                  alt="Icons Images"
-                  width="100"
-                  height="100"
-                  src={CounterImg1}
-                />
-              </div>
-              <div className="content">
-                <span style={counterValueStyle}>{pageData.workshops}</span>
-                <span className="subtitle">Active Events</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-4 col-sm-6 col-12 ">
-          <div className="rbt-counterup rbt-hover-03">
-            <div className="inner">
-              <div className="icon">
-                <ImageTag
-                  alt="Icons Images"
-                  width="100"
-                  height="100"
-                  src={CounterImg}
-                />
-              </div>
-              <div className="content">
-                <span style={counterValueStyle}>
-                  {pageData.revenue ? pageData.revenue.length : 0}
-                </span>
-                <span className="subtitle">Monthly Revenue</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    </div>
 }
