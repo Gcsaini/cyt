881c7af8a342861287d76127ae295b140c2cca6a
Deepak <dpk7697@gmail.com>
2025-09-09 16:20:18 +0530

    updated content on <home>

--- src/components/therapists/dashboard/performance.js
+++ src/components/therapists/dashboard/performance.js
@@ -1,6 +1,6 @@
 import { useMediaQuery } from "@mui/material";
 
-export default function NotifyBar({ title = "Good to see you here." }) {
+export default function NotifyBar({ title = "Dashboard currently under development" }) {
     const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
     return <div class="rbt-header-top-2 color-white pt--15 pb--15 mb--15" style={{ backgroundColor: "rgba(43, 194, 76, 1)", borderRadius: "6px", marginTop: isMobile ? '20px' : 0 }}>
         <div class="container">
