718954d4d227f6f4e8d631fe4c9523a0049105bc
Deepak <dpk7697@gmail.com>
2025-09-11 05:39:27 +0530

    updated content on <home>

--- src/components/home/state.js
+++ src/components/home/state.js
@@ -1,40 +1,52 @@
 import React from "react";
-import BlogCardImg2 from "../../assets/img/web-design736e.jpg";
+import GujaratImg from "../../assets/img/Gujrat.png";
+import UttarakhandImg from "../../assets/img/Uttarakhand.png";
+import DelhiImg from "../../assets/img/Delhi.png";
+import RajasthanImg from "../../assets/img/Rajasthan.png";
 import LazyImage from "../../utils/lazy-image";
 
 export default function State() {
-  const categories = Array.from({ length: 8 }, (_, index) => ({
-    title: `Category ${index + 1}`,
-    link: "#",
-    courses: 1,
-    img: BlogCardImg2,
-  }));
+  const statesToShow = [
+    { name: "Uttarakhand", img: UttarakhandImg },
+    { name: "Gujarat", img: GujaratImg },
+    { name: "Rajasthan", img: RajasthanImg },
+    { name: "Delhi", img: DelhiImg },
+  ];
 
   return (
     <div className="rbt-category-area bg-color-white" style={{ padding: "50px 0" }}>
       <div className="container">
+        {/* Heading */}
+        <div className="row mb-4">
+          <div className="col-12 text-center">
+            <h2 className="title"> Explore Therapists in Different States </h2>
+            <h2 style={{ fontSize: "2rem", fontWeight: "600" }}> Available in 04 States </h2>
+          </div>
+        </div>
+
+        {/* Categories */}
         <div className="row g-4">
-          {categories.map((category, index) => (
+          {statesToShow.map((state, index) => (
             <div key={index} className="col-xl-3 col-lg-3 col-md-6 col-sm-12">
               <a
                 className="rbt-cat-box rbt-cat-box-1 image-overlaping-content on-hover-content-visible"
-                href={category.link}
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  height: "100%",
-                  textDecoration: "none",
-                }}
+                href="#"
+                style={{ display: "flex", flexDirection: "column", height: "100%", textDecoration: "none" }}
               >
                 <div className="inner" style={{ flex: 1, display: "flex", flexDirection: "column" }}>
                   <div className="thumbnail" style={{ marginBottom: 15, flexShrink: 0 }}>
-                    <LazyImage alt={category.title} dim={"250"} src={category.img} style={{ width: "100%", height: "auto", objectFit: "cover" }} />
+                    <LazyImage
+                      alt={state.name}
+                      dim={"250"}
+                      src={state.img}
+                      style={{ width: "100%", height: "auto", objectFit: "cover" }}
+                    />
                   </div>
                   <div className="content" style={{ textAlign: "center", marginTop: "auto" }}>
-                    <h5 className="title">{category.title}</h5>
+                    <h5 className="title">{state.name}</h5>
                     <div className="read-more-btn">
                       <span className="rbt-btn-link">
-                        {category.courses} Courses <i className="feather-arrow-right"></i>
+                        1 Course <i className="feather-arrow-right"></i>
                       </span>
                     </div>
                   </div>
