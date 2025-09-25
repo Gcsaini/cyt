aaabd9efff86d466ac94a579f9247d0e1e8455a5
Deepak <dpk7697@gmail.com>
2025-09-25 22:17:53 +0530

    updated content on <home>

--- src/components/home/banner.js
+++ src/components/home/banner.js
@@ -53,7 +53,7 @@ export default function Banner() {
     <section
       className="rbt-banner-area rbt-banner-1"
       style={{
-        paddingTop: isMobile ? "60px" : "80px",
+        paddingTop: isMobile ? "60px" : "0px",
         paddingBottom: isMobile ? "40px" : "100px",
       }}
     >
