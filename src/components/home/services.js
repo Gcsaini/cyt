82dfe0889ddd82effdab03b29c483803ef8136dd
Deepak <dpk7697@gmail.com>
2025-09-27 13:45:54 +0530

    updated content on <home>

--- src/components/home/services.js
+++ src/components/home/services.js
@@ -87,7 +87,7 @@ export default function Services() {
 
       <div
         className="rbt-categories-area bg-color-white rbt-section-gapBottom"
-        style={{ marginTop: 80 }}
+        style={{ marginTop: 10 }}
       >
         <div className="container">
           <div className="row">
